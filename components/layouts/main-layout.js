f8b49df0920d2fad31a4910fe8572606e06a9b32
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2022-06-17 17:31:13 +0430

    Added fullPage

--- components/layouts/main-layout.js
+++ components/layouts/main-layout.js
@@ -3,9 +3,7 @@ import React from "react";
 export default function main({ children }) {
   return (
     <div dir="rtl">
-      <div>header</div>
       <div>{children}</div>
-      <div>footer</div>
     </div>
   );
 }

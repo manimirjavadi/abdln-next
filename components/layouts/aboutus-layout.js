9d213ffc50321b1f89b0c35a73df1062e3dcdb30
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2022-06-24 20:40:32 +0430

    Fixed aboutUs page

--- components/layouts/aboutus-layout.js
+++ components/layouts/aboutus-layout.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Header } from "../UI/Header";
 import { Footer } from "../../components/Footer/Footer";
+import Head from "next/head";
 
 export default function aboutus({ children }) {
   return (
     <div dir="rtl" className="mx-auto">
+      <Head>
+        <title>عبداللهیان</title>
+        <meta name="description" content="وبسایت رسمی عبداللهیان" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header hideHeader={true} otherPages={false} />
       <div className="h-[100px]"></div>
       <div>{children}</div>

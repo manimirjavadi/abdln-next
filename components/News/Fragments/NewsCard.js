d9bd1bd33e099d9f8aeda007e4eec92d13339e0c
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2022-06-25 23:10:19 +0430

    Changed the design of newscard

--- components/News/Fragments/NewsCard.js
+++ components/News/Fragments/NewsCard.js
@@ -4,11 +4,8 @@ import { motion } from "framer-motion";
 
 export const NewsCard = () => {
   return (
-    <motion.div
-      whileHover={{ scale: 1.1 }}
-      className="px-6 py-10 cursor-pointer"
-    >
-      <div className="newsCard relative">
+    <div className="cursor-pointer">
+      <div className="newsCard h-full relative">
         {/* News Title */}
         <div className="p-5">
           <h4 className="text-black">خاک دیاتومه</h4>
@@ -17,17 +14,18 @@ export const NewsCard = () => {
         {/* Product Image */}
         <div className="p-5 w-full h-full">
           <Image
+            className="hover:blur-sm duration-200 shadow-lg"
             alt="خاک دیاتومه"
-            src="/tempImgs/newback.jpg"
+            src="/tempImgs/4.jpg"
             height="100%"
             width="100%"
             layout="fill"
             objectFit="cover"
           />
           {/* Product description */}
-          <div className="absolute bottom-[-14px] right-[-10px] w-[110%] h-24 bg-white blur-lg" />
+          <div className="absolute bottom-[-14px] right-[-10px] w-[110%] h-32 bg-white blur-lg" />
           <div className="absolute w-full bottom-0 right-0 px-2 pb-3">
-            <p className="text-black font-bold w-full line-clamp-2">
+            <p className="text-black text-xl font-bold w-full line-clamp-3">
               خاک دیاتومه (Diatomaceous earth) از بقایای نوعی جلبک تک‌سلولی به
               نام دیاتوم به‌دست‌آمده است. دیاتوم‌ها گروهی از جلبک‌های کوچک هستند
               که در اقیانوس‌ها، آبراه‌ها و خاک‌های سراسر جهان یافت می‌شوند.
@@ -35,6 +33,6 @@ export const NewsCard = () => {
           </div>
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
